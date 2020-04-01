e93ec15b7d87c292e054f1fa9e596a4f5a082cc0
fredlc <fredlefevre@outlook.com>
2020-04-01 13:30:00 -0400

    Created UIColor and UIView extensions

---
+++ InstagramFirebase/Utils/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  InstagramFirebase
+//
+//  Created by Fred Lefevre on 2020-04-01.
+//  Copyright © 2020 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
